e859361493c6d6d33d9bf4c828a37210f110a28a
Bryan Kneis <bryan.kneis@loft.sh>
2025-01-22 14:50:19 +0000

    Remove copyright

--- src/tailscale-userspace/tailscaled-entrypoint.sh
+++ src/tailscale-userspace/tailscaled-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
 
 if [[ "$(id -u)" -eq 0 ]]; then
   mkdir -p /workspaces/.tailscale
